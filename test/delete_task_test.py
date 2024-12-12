456abe0788949686001ac47b9d706a37c666b42b
Perfecto1682 <perfecto_honrado5@mail.ru>
2024-12-12 20:14:54 +0300

    Fix code

--- test/delete_task_test.py
+++ test/delete_task_test.py
@@ -1,11 +1,12 @@
+import allure
 import requests
 
 
+@allure.title('Удаление задачи')
 def test_delete():
     body = {"title": "generated", "completed": False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
-    id = response.json()["id"]
 
+    response.json().get("id")
     response = requests.delete(f'https://todo-app-sky.herokuapp.com/{id}')
-
     assert response.status_code == 204
--- test/edit_task_test.py
+++ test/edit_task_test.py
@@ -1,13 +1,15 @@
 import requests
+import allure
 
 
+@allure.title('Редактирование задачи')
 def test_edit():
     body = {"title": "generated", "completed": False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
-    id = response.json()["id"]
+    response.json().get("id")
 
     body = {"title": "generated-1"}
-    response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
+    response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)  # noqa: E501
     assert response.status_code == 200
 
     response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
