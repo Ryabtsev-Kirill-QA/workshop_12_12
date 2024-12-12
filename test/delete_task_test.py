7c6faa70f2d8e10dc57d2c21a276fd8c086ea8a0
Kirill <ryabtsev231297@gmail.com>
2024-12-12 20:54:52 +0300

    fix id

--- test/delete_task_test.py
+++ test/delete_task_test.py
@@ -19,7 +19,7 @@ def test_delete_negative():
     with allure.step("Отправляем валидный запрос на создание задания"):
         body = {"title": "generated", "completed": False}
         response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
-        id = 123123123123
+        id = 123
     with allure.step("Отправляем валидный запрос на создание задания"):
         response = requests.delete(f'https://todo-app-sky.herokuapp.com/{id}')
 
